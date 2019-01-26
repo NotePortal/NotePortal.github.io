588fb60544eeb267f886eaad43a0e6ba764c0402
NotePortal <43442308+NotePortal@users.noreply.github.com>
2019-01-26 23:10:55 +0200

    Add files via upload

--- 184290/main.js
+++ 184290/main.js
@@ -1,41 +1,13 @@
-window.onload = function() {
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+|| navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-    // Normalize the various vendor prefixed versions of getUserMedia.
-    navigator.getUserMedia = (navigator.getUserMedia ||
-                              navigator.webkitGetUserMedia ||
-                              navigator.mozGetUserMedia || 
-                              navigator.msGetUserMedia);
-  
-  }
 
-  // Check that the browser supports getUserMedia.
-// If it doesn't show an alert, otherwise continue.
 if (navigator.getUserMedia) {
-    // Request the camera.
-    navigator.getUserMedia(
-      // Constraints
-      {
-        video: true
-      },
-  
-      // Success Callback
-      function(localMediaStream) {
-  
-      },
-  
-      // Error Callback
-      function(err) {
-        // Log the error to the console.
-        console.log('The following error occurred when trying to use getUserMedia: ' + err);
-      }
-    );
-  
-  } else {
-    alert('Sorry, your browser does not support getUserMedia');
-  }
-  // Get a reference to the video element on the page.
-var vid = document.getElementById('camera-stream');
-
-// Create an object URL for the video stream and use this 
-// to set the video source.
-vid.src = window.URL.createObjectURL(localMediaStream);
+    navigator.getUserMedia({video: true, audio: false}, handleVideo, videoError);
+};
+function handleVideo(stream) {
+    document.querySelector("#video").src = window.URL.createObjectURL(stream);
+};
+function videoError(e) {
+    alert("Problems with access to 'Camera': "+e);
+};
