ae8a81fb54d2fad9fb7dc2168854c0940f42749d
NotePortal <43442308+NotePortal@users.noreply.github.com>
2019-01-27 10:51:21 +0200

    Add files via upload

--- 184290/main.js
+++ 184290/main.js
@@ -1,13 +1,25 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
 || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
+var video = document.querySelector("#video");
+var canvas = document.querySelector("#canvas");
+var c = canvas.getContext("2d");
 
 if (navigator.getUserMedia) {
-    navigator.getUserMedia({video: true, audio: false}, handleVideo, videoError);
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
 };
 function handleVideo(stream) {
-    document.querySelector("#video").src = window.URL.createObjectURL(stream);
+    video.src = window.URL.createObjectURL(stream);
 };
 function videoError(e) {
     alert("Problems with access to 'Camera': "+e);
+    document.getElementById("log").innerHTML = "Problems with access to 'Camera': "+e;
 };
+console.log(window.console);
+function snap() {
+    canvas.width = video.width;
+    canvas.height = video.height;
+    c.drawImage(video, 0, 0,);
+    window.requestAnimationFrame(snap);
+};
+setTimeout(snap, 100);
